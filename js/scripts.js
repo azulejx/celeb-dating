9f19ee612f21fa4e7ca4735247437b9a6db277f4
Azul Rodriguez Da Silva and Amelia Ralston-Okabayashi <azul@>
2018-09-17 20:36:22 -0700

    add the javascript for results and add the logic for our quiz, also problem solved syntax

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,37 @@
-$(document).ready(function () {
-  
+
+$(document).ready(function() {
+  $("form#form").submit(function(event) {
+    event.preventDefault();
+
+    var age = parseInt($(".age").val());
+    var food = parseInt($(".food").val());
+    var movie = parseInt($(".movie").val());
+    var hobby = parseInt($(".hobby").val());
+    var type = parseInt($(".type").val());
+    var results = food + movie + hobby + type;
+    //yoshi results
+    if(results === 0 || results < 4){
+      $("#yoshi").show();
+    }
+    //mulan results
+    else if(results >= 4 && results < 8){
+      $("#mulan").show();
+    }
+    //pikachu results
+    else if(results >= 8 && results < 12){
+      $("#pikachu").show();
+    }
+    //jonny bravo results
+    else if(results >= 12 && results < 16){
+      $("#jonnybravo").show();
+    }
+    //princess kida
+    else if(results >= 16){
+      $("#princesskida").show();
+    }
+    else{
+      $("#error").show();
+    }
+  });
 
 });
